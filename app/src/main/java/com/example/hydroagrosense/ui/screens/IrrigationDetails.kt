9174777f46b979e892e5fcba643290329cbb850a
Jorge Alcaras <up220194@alumnos.upa.edu.mx>
2025-11-12 02:03:49 -0600

    feat(Irrigation Details): :sparkles: The irrigation details screen has been created

---
+++ app/src/main/java/com/example/hydroagrosense/ui/screens/IrrigationDetails.kt
@@ -0,0 +1,294 @@
+package com.example.hydroagrosense.ui.screens
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.hydroagrosense.data.Measure
+import com.example.hydroagrosense.data.OptimalMeasure
+import com.example.hydroagrosense.ui.components.MeasureDetailsRow
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun IrrigationDetails() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = Color(0xFFEDEDED),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Irrigation Details",
+                        fontSize = 28.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.Black,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFFE3E3E3),
+                    titleContentColor = Color.Black,
+                    navigationIconContentColor = Color.Black
+                )
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xFFFFFFFF),
+                    contentColor = Color.Black,
+                    disabledContainerColor = Color.Gray,
+                    disabledContentColor = Color.DarkGray
+                ),
+                border = BorderStroke(color = Color(0xFFFFFFFF), width = 1.dp),
+            ) {
+                Text(
+                    text = "Irrigation Event",
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = "Date: 10/10/2023",
+                    fontSize = 16.sp,
+                    color = Color.DarkGray,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier.padding(start = 16.dp, bottom = 2.dp)
+                )
+                Text(
+                    text = "Duration: 30 minutes",
+                    fontSize = 16.sp,
+                    color = Color.DarkGray,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier.padding(start = 16.dp, bottom = 2.dp)
+                )
+                Text(
+                    text = "Reaeson: Soil moisture below optimal level",
+                    fontSize = 16.sp,
+                    color = Color.DarkGray,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier.padding(start = 16.dp, bottom = 16.dp)
+                )
+
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xFFFFFFFF),
+                    contentColor = Color.Black,
+                    disabledContainerColor = Color.Gray,
+                    disabledContentColor = Color.DarkGray
+                ),
+                border = BorderStroke(color = Color(0xFFFFFFFF), width = 1.dp),
+            ) {
+                Text(
+                    text = "Before Irrigation",
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                MeasureDetailsRow(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    barMax = 100.0,
+                    measure = Measure(
+                        id = 2,
+                        type = "Soil Moisture",
+                        value = 32.0,
+                        unit = "%",
+                        timestamp = System.currentTimeMillis(),
+                        optimalMeasure = OptimalMeasure(
+                            id = 2,
+                            parameter = "Soil Moisture",
+                            optimalMinValue = 40.0,
+                            optimalMaxValue = 60.0,
+                            unit = "%",
+                        )
+                    )
+                )
+                MeasureDetailsRow(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    barMax = 100.0,
+                    measure = Measure(
+                        id = 1,
+                        type = "Air Humidity",
+                        value = 35.0,
+                        unit = "%",
+                        timestamp = System.currentTimeMillis(),
+                        optimalMeasure = OptimalMeasure(
+                            id = 1,
+                            parameter = "Air Humidity",
+                            optimalMinValue = 50.0,
+                            optimalMaxValue = 70.0,
+                            unit = "%",
+                        )
+                    )
+
+                )
+                MeasureDetailsRow(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    barMax = 50.0,
+                    measure = Measure(
+                        id = 3,
+                        type = "Temperature",
+                        value = 37.0,
+                        unit = "°C",
+                        timestamp = System.currentTimeMillis(),
+                        optimalMeasure = OptimalMeasure(
+                            id = 3,
+                            parameter = "Temperature",
+                            optimalMinValue = 20.0,
+                            optimalMaxValue = 25.0,
+                            unit = "°C",
+                        )
+                    )
+                )
+                MeasureDetailsRow(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    barMax = 100.0,
+                    measure = Measure(
+                        id = 4,
+                        type = "Light Intensity",
+                        value = 85.0,
+                        unit = "%",
+                        timestamp = System.currentTimeMillis(),
+                        optimalMeasure = OptimalMeasure(
+                            id = 4,
+                            parameter = "Light Intensity",
+                            optimalMinValue = 60.0,
+                            optimalMaxValue = 100.0,
+                            unit = "%",
+                        )
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xFFFFFFFF),
+                    contentColor = Color.Black,
+                    disabledContainerColor = Color.Gray,
+                    disabledContentColor = Color.DarkGray
+                ),
+                border = BorderStroke(color = Color(0xFFFFFFFF), width = 1.dp),
+            ) {
+                Text(
+                    text = "After Irrigation",
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                MeasureDetailsRow(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    barMax = 100.0,
+                    measure = Measure(
+                        id = 2,
+                        type = "Soil Moisture",
+                        value = 45.0,
+                        unit = "%",
+                        timestamp = System.currentTimeMillis(),
+                        optimalMeasure = OptimalMeasure(
+                            id = 2,
+                            parameter = "Soil Moisture",
+                            optimalMinValue = 40.0,
+                            optimalMaxValue = 60.0,
+                            unit = "%",
+                        )
+                    )
+                )
+                MeasureDetailsRow(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    barMax = 100.0,
+                    measure = Measure(
+                        id = 1,
+                        type = "Air Humidity",
+                        value = 55.0,
+                        unit = "%",
+                        timestamp = System.currentTimeMillis(),
+                        optimalMeasure = OptimalMeasure(
+                            id = 1,
+                            parameter = "Air Humidity",
+                            optimalMinValue = 50.0,
+                            optimalMaxValue = 70.0,
+                            unit = "%",
+                        )
+                    )
+
+                )
+                MeasureDetailsRow(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    barMax = 50.0,
+                    measure = Measure(
+                        id = 3,
+                        type = "Temperature",
+                        value = 22.0,
+                        unit = "°C",
+                        timestamp = System.currentTimeMillis(),
+                        optimalMeasure = OptimalMeasure(
+                            id = 3,
+                            parameter = "Temperature",
+                            optimalMinValue = 20.0,
+                            optimalMaxValue = 25.0,
+                            unit = "°C",
+                        )
+                    )
+                )
+                MeasureDetailsRow(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    barMax = 100.0,
+                    measure = Measure(
+                        id = 4,
+                        type = "Light Intensity",
+                        value = 70.0,
+                        unit = "%",
+                        timestamp = System.currentTimeMillis(),
+                        optimalMeasure = OptimalMeasure(
+                            id = 4,
+                            parameter = "Light Intensity",
+                            optimalMinValue = 60.0,
+                            optimalMaxValue = 100.0,
+                            unit = "%",
+                        )
+                    )
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+}
